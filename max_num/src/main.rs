b2d9b71bf45656683076d0d8ab5808ae9f987f67
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2023-09-16 16:23:40 +0200

    a

--- max_num/src/main.rs
+++ max_num/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
     let mut x: f64 = 1.0;
-    const inf: f64 = 2.0;
     loop {
-        x = x * 1.1;
+        x = x * 1.01;
         println!("{x}");
-        if x == inf {
-            break;
-        }
     }
 }
